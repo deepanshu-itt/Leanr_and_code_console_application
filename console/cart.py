80a1e8dbad57856434634a742a211ac12fc71b26
Deepanshu Pariyani <deepanshu.p@intimetec.com>
2025-03-25 21:39:15 +0530

    Followed Top down Approach, SRP and Single Entry Exit Principle

--- console/cart.py
+++ console/cart.py
@@ -10,12 +10,12 @@ def add_To_Cart(product_id):
     quantity = int(input("Enter quantity: "))
     response = requests.post(f'{BASE_URL}/add_to_cart', json={'product_id': product_id, 'quantity': quantity})
     
-    print(f"Response Status Code: {response.status_code}")
-    print(f"Response Content: {response.text}")
+    is_Product_Added_To_Cart(response)
+    return 
     
-    if response.status_code == 201:
-        print("Product added to cart!")
-    elif response.status_code == 200:
+
+def is_Product_Added_To_Cart(response):
+    if response.status_code == 201 or response.status_code == 200:
         print("Product added to cart!")
     else:
         print("Failed to add to cart.")
@@ -24,21 +24,8 @@ def add_To_Cart(product_id):
             print(response_data.get('message', 'No message in response'))
         except ValueError:
             print("Response is not in JSON format.")
-
-
-def remove_Product_From_Cart(product_id):
-    quantity_to_remove = int(input("Enter quantity to remove: "))
     
-    if quantity_to_remove <= 0:
-        print("Quantity to remove must be greater than 0.")
-        return
-    
-    response = requests.put(f'{BASE_URL}/update_cart_quantity', json={'product_id': product_id, 'quantity_to_remove': quantity_to_remove})
-    
-    if response.status_code == 200:
-        print("Product quantity updated successfully.")
-    else:
-        print("Failed to update product quantity in the cart.")
+    return
 
 
 def view_Cart():
@@ -54,15 +41,19 @@ def view_Cart():
                 remove_Product_From_Cart(product_id)
             except ValueError:
                 print("Invalid product ID.")
+    
+    return
 
 
 def get_Cart_Items():
     response = requests.get(f'{BASE_URL}/view_cart')
+    cart_Items = None
     if response.status_code == 200:
-        return response.json()
+        cart_Items = response.json()
     else:
         print("Failed to fetch cart items.")
-        return None
+    
+    return cart_Items
 
 
 def display_Cart(cart_items):
@@ -73,7 +64,30 @@ def display_Cart(cart_items):
         print("\nTotal Amount: ", cart_items['total_amount'])
     else:
         print("Your cart is empty.")
+    
+    return
+
 
 def get_Remove_Choice():
     remove_choice = input("Enter product ID to reduce quantity or remove (or press Enter to skip): ")
     return remove_choice
+
+
+def remove_Product_From_Cart(product_id):
+    quantity_to_remove = int(input("Enter quantity to remove: "))
+    
+    if quantity_to_remove > 0:
+        response = requests.put(f'{BASE_URL}/update_cart_quantity', json={'product_id': product_id, 'quantity_to_remove': quantity_to_remove})
+        is_Product_Updated_In_Cart(response)
+    else:
+        print("Quantity to remove must be greater than 0.")
+    
+    return
+
+
+def is_Product_Updated_In_Cart(response):
+    if response.status_code == 200:
+        print("Product quantity updated successfully.")
+    else:
+        print("Failed to update product quantity in the cart.")
+    return
